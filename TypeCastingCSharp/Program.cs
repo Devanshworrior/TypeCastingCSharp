2c590477b92ccb091ca747cf488abc995e5d366e
Devanshworrior <devanshjangid269@gmail.com>
2024-04-16 20:38:12 +0530

    first commit

---
+++ TypeCastingCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿//1. Impliciusing System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCastingCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Implicit Casting [char -> int -> long -> float -> double]
+            int a = 9;
+            double b = a;
+            Console.WriteLine(b);
+
+            //2. Explicit Casting [double -> float -> long -> int -> char]
+            double c = 9.78;
+            int myInt = (int)c;   
+            Console.WriteLine(c);   
+            Console.WriteLine(myInt);
+            Console.ReadLine();
+        }
+    }
+}
